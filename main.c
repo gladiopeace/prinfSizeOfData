21e374f5261dbb27c76067483d4d21da577e43a8
gladiopeace <gladiopeace@gmail.com>
2013-11-18 05:05:37 -0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+void main()
+{
+printf("Type\t\t\t\tSize (bytes)");
+printf("\nCharacter\t\t\t    %d",sizeof(char));
+printf("\nInteger\t\t\t\t    %d",sizeof(int));
+printf("\nLong int\t\t\t    %d",sizeof(long int));
+printf("\nFloat\t\t\t\t    %d",sizeof(float));
+printf("\nDouble\t\t\t\t    %d",sizeof(double));
+printf("\nLong double\t\t\t    %d",sizeof(long double));
+getch();
+}
